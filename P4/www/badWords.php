36f9959b4395136a40e4c3461c62e2b351016a20
Marina Muñoz <marinamuca01@gmail.com>
2022-05-09 16:12:34 +0200

    Duplicate P3

--- P4/www/badWords.php
+++
@@ -1,9 +0,0 @@
-<?php
- require_once "/usr/local/lib/php/vendor/autoload.php";
- include("bd.php");
-
- $msqli = dbConnect();
- header('Content-type: application/json; charset=utf-8');
- echo json_encode(getBadWords($msqli), JSON_FORCE_OBJECT);
-    
-?>
